64396137316a3862eae271ac692605cb32a63d5e
aleclerc <aleclerc>
2015-10-01 17:03:29 +0200

    refactoring

--- raspMusic-storage/src/main/java/fr/motaz/rasp/music/storage/StorageService.java
+++ raspMusic-storage/src/main/java/fr/motaz/rasp/music/storage/StorageService.java
@@ -9,22 +9,21 @@ import java.util.List;
 import fr.motaz.rasp.music.model.Music;
 
 public class StorageService {
-	public List<Music> getMusicList() throws Exception{
+	public List<Music> getMusicList() throws Exception {
 		List<Music> musics = new ArrayList<Music>();
 		Files.walk(Paths.get(RaspConf.getPropValue("music.folder.path"))).forEach(filePath -> {
-		    if (Files.isRegularFile(filePath)) {
-		    	System.out.println(filePath.toUri());
-		    	try {
+			if (Files.isRegularFile(filePath)) {
+
+				try {
 					musics.add(new Music(new File(filePath.toUri())));
-					System.out.println("add");
+
 				} catch (Exception e) {
-					System.out.println("exception");
+
 				}
-		    }
+			}
 		});
 		return musics;
-		
+
 	}
-	
-	
+
 }
