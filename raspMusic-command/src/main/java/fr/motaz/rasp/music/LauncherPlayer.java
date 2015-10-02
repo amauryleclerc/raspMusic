aebe851196ea08a2fd87645b4b6da42c052e3492
aleclerc <aleclerc>
2015-10-02 09:30:16 +0200

    refactoring

---
+++ raspMusic-command/src/main/java/fr/motaz/rasp/music/LauncherPlayer.java
@@ -0,0 +1,40 @@
+package fr.motaz.rasp.music;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import fr.motaz.rasp.music.model.Music;
+import fr.motaz.rasp.music.player.Player;
+
+public class LauncherPlayer {
+	private static Player player;
+
+	public static void main(String[] args) throws Exception {
+	
+		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[] { "classpath:applicationContext-player.xml" })) {
+			player = (Player) context.getBean("player");
+			Music music = (Music) context.getBean("music");
+			Music music2 = (Music) context.getBean("music");
+			// File file = new File("/Users/Ludovic/Desktop/test.mp3");
+//			File file = new File("D:\\musique\\musique_itunes\\Music\\Bonobo\\Black Sands\\1-02 Kiara.mp3");
+//			File file2 = new File("D:\\musique\\musique_itunes\\Music\\Bonobo\\Late Night Tales (Mixed)\\05 Baltimore.mp3");
+			music.setPath("D:\\musique\\musique_itunes\\Music\\Bonobo\\Black Sands\\1-02 Kiara.mp3");
+			music2.setPath("D:\\musique\\musique_itunes\\Music\\Bonobo\\Late Night Tales (Mixed)\\05 Baltimore.mp3");
+			player.getPlaylist().add(music);
+			player.play();
+			Thread.sleep(2000);
+			player.pause();
+			Thread.sleep(2000);
+			player.play();
+			player.getPlaylist().add(music2);
+			Thread.sleep(2000);
+			player.next();
+			Thread.sleep(2000);
+			player.previous();
+			Thread.sleep(2000);
+			player.stop();
+		}
+
+	}
+
+}
